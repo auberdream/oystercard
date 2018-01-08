8e48bfc0665c5e8de20893bdb4c6e9e033754049
Ellie <elliewem@gmail.com>
2018-01-08 14:48:43 +0000

    Adds balance to initialize method and attr_reader

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,13 @@
+require 'oystercard'
 
-describe Oyster do
+describe Oystercard do
+
+  describe "#balance" do
+
+    it 'initializes with a balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+
+  end
 
 end

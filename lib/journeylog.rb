79e0bdd2053053ba1c832c96d9d7a9c28be0ee2a
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-12 14:00:11 +0000

    start method in journeylog

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -1,6 +1,13 @@
 class Journeylog
+  attr_reader :journey
 
   def initialize
+    @journey = nil
+  end
+
+  def start(station)
+    @journey = Journey.new
+    @journey.start(station)
   end
 
 end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -1,7 +1,15 @@
 require 'journeylog'
 
 describe Journeylog do
+  let(:journey) { double(:journey) }
+  let(:entry_station) { double(:station) }
 
+  describe '#start'do
+    it 'creates a new journey' do
+      allow(Journey).to receive(:new).and_return(journey)
+      allow(journey).to receive(:start)
+      expect{ subject.start(entry_station) }.to change { subject.journey }.to(journey)
+    end
 
-
+  end
 end
